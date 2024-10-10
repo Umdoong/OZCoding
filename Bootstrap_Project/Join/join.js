c7cde7375e5ef14f6c654660600280130fdbe0de
hyo <z3375@naver.com>
2024-10-10 17:58:14 +0900

    feat : Admin 페이지 프론트단 제작 프로젝트 1일차

---
+++ Bootstrap_Project/Join/join.js
@@ -0,0 +1,68 @@
+//제출 이벤트를 받는다(이벤트 핸들링)
+//제출된 입력 값들을 참조한다
+// 입력값에 문제가 있는 경우 이를 감지한다
+//가입 환영 인사를 제공한다
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(event){
+	event.preventDefault() // 새로고침 차단
+
+	let userId = event.target.id.value
+	let userPw1 = event.target.pw1.value
+	let userPw2 = event.target.pw2.value
+	let userName = event.target.name.value
+	let userPhone = event.target.phone.value
+	let userGender = event.target.gender.value
+	let userEmail = event.target.email.value
+
+	//아이디 정규식 검사
+	var idReg = /^[a-zA-Z]+[a-z0-9]{5,11}$/g;
+	if( !idReg.test(userId) ) {
+		alert("아이디는 영문자로 시작하는 6~12자 영문자 또는 숫자이어야 합니다.");
+		return;
+	}
+
+	//비밀번호 정규식 검사
+	var passwordReg = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+	if (passwordReg.test(userPw1)) {
+		if (userPw1 === userPw2) {
+			alert('비밀번호가 서로 일치합니다.')
+		}else{
+			alert('비밀번호가 서로 일치하지 않습니다.')
+			return
+		}
+	}else{
+		alert('비밀번호는 최소 8자에서 30자까지, 영문자, 숫자 및 특수 문자를 포함해야 합니다.')
+		return
+	}
+
+	//폰 정규식 검사
+	var phoneReg = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/g;
+	if(!phoneReg.test(userPhone)){
+		alert("올바른 형식의 휴대전화 번호가 아닙니다")
+		return
+	}
+
+	//알림창 띄우기
+	info=
+		`${userId}님 환영합니다.회원 가입 시 입력하신 내역은 다음과 같습니다.
+		아이디 : ${userId}
+		이름 : ${userName}
+		전화번호 : ${userPhone}
+		성별 : ${userGender}
+		이메일 : ${userEmail}`;
+	
+	alert(info.replace(/\t/g, ''))
+
+	//가입 잘 되었다!
+	document.body.innerHTML = ""
+	document.write(`<p>${userId}님 가입을 환영합니다.</p>`)
+})
+
+//폰 자동 하이픈
+const autoHyphen = (phone) => {
+    phone.value = phone.value
+    .replace(/[^0-9]/g, '')
+    .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+}
