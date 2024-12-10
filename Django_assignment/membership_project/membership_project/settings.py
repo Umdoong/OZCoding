f715373bfd994202aaabf084136effd8dc08a236
hyo <umdoongs@gmail.com>
2024-12-10 16:54:31 +0900

    ✨ Django 3일차 과제
    Django 3일차 과제

--- Django_assignment/membership_project/membership_project/settings.py
+++ Django_assignment/membership_project/membership_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uq0tb$d(5vzd2skur*v^)m)jyl4n+bty)lm8*!-icmo_gf10kj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
